2a342c7f77058dd13e549416d92a430403880e4c
Salman Faris <salmanfs953@gmail.com>
2024-09-21 17:48:22 +0530

    8th commit

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -363,7 +363,7 @@ console.log(user,'nnnnnnnn');
             subject: 'Password Reset',
           text:` You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n +
                 Please click on the following link, or paste this into your browser to complete the process:\n\n +
-                http://localhost:3000/resetPassword/${resetToken}\n\n +
+                https://malefashions.shop/resetPassword/${resetToken}\n\n +
                 If you did not request this, please ignore this email and your password will remain unchanged.\n`
         };
         await transport.sendMail(mailOption)
